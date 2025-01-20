9bdc8010312041ae3368ba50cc480ebd94c8617d
Henrik408 <henrik.mayer@yahoo.com>
2025-01-20 12:20:00 +0100

    Montag

--- extra1.py
+++ extra1.py
@@ -1,3 +1,40 @@
-products = ['table', 'chair', 'door']
+""" products = ['table', 'chair', 'door']
 for i, item in enumerate(products):
     print("Product: ", item)
+ """
+
+""" seconds = [1.23, 1.45, 1.02, 1.11]
+seconds.pop(1)
+print(seconds) """
+
+
+""" mystring = "hello world" 
+mystring = mystring.capitalize()
+print(mystring)
+mylist =  [1,2,3,4,5,6,7,8,9]
+print(len(mystring))
+print(len(mylist)) """
+
+""" mylist = ['a', 'b', 'c', 'd']
+for item in mylist:
+    print(len(mylist)) """
+
+""" waiting_lsit = ["sen", "ben","john"]
+waiting_lsit.sort()
+for i,item in enumerate(waiting_lsit):
+    print(f"{i + 1}. {item.capitalize()}") """
+
+""" measurements = [177.8, 175.8, 166.9, 182.5]
+measurements.sort()
+for item in measurements:
+    print(item) """
+
+menu = ["pasta", "pizza", "salad"]
+ 
+# for i, j in enumerate(menu):
+#     print(i,j)
+#     print(f"{i}.{j}")
+
+buttons = [('John', 'Sen', 'Morro'), ('Lin', 'Ajay', 'Filip')]
+for first, second, third in buttons:
+    print(first, second, third)
